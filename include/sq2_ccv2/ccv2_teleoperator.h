a6d0661598ca25f944b7b27c805b0ea518bdfa2f
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-12-19 14:08:10 +0900

    add ros teleoperation

--- include/sq2_ccv2/ccv2_teleoperator.h
+++ include/sq2_ccv2/ccv2_teleoperator.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
+#include <geometry_msgs/Twist.h>
 
 #include "ypspur_mqtt/velocity_data.h"
 
@@ -20,17 +21,22 @@ private:
     // axes
     static constexpr int L_STICK_H = 0;
     static constexpr int L_STICK_V = 1;
-    static constexpr int R_STICK_H = 2;
-    static constexpr int L2_STICK = 3;
-    static constexpr int R2_STICK = 4;
-    static constexpr int R_STICK_V = 5;
+    static constexpr int L2_STICK = 2;
+    static constexpr int R_STICK_H = 3;
+    static constexpr int R_STICK_V = 4;
+    static constexpr int R2_STICK = 5;
     // buttons
     static constexpr int L1 = 4;
     static constexpr int L2 = 6;
     static constexpr int R2 = 7;
 
+    double MAX_VELOCITY;
+    double MAX_ANGULAR_VELOCITY;
+    double MAX_STEERING_ANGLE;
+
     ros::NodeHandle nh;
     ros::NodeHandle local_nh;
+    ros::Publisher cmd_vel_pub;
     ros::Subscriber joy_sub;
 
     // MQTT
--- src/ccv2_teleoperator.cpp
+++ src/ccv2_teleoperator.cpp
@@ -4,6 +4,12 @@ CCV2Teleoperator::CCV2Teleoperator(void)
 :local_nh("~")
 {
     std::cout << "=== ccv2_teleoperator ===" << std::endl;
+
+    cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    joy_sub = nh.subscribe("joy", 1, &CCV2Teleoperator::joy_callback, this, ros::TransportHints().tcpNoDelay());
+    local_nh.param<double>("MAX_VELOCITY", MAX_VELOCITY, {1.0});
+    local_nh.param<double>("MAX_ANGULAR_VELOCITY", MAX_ANGULAR_VELOCITY, {M_PI});
+    local_nh.param<double>("MAX_STEERING_ANGLE", MAX_STEERING_ANGLE, {M_PI / 12.0});
 }
 
 void CCV2Teleoperator::process(void)
@@ -13,5 +19,30 @@ void CCV2Teleoperator::process(void)
 
 void CCV2Teleoperator::joy_callback(const sensor_msgs::JoyConstPtr& msg)
 {
-    YPSpurWrapper::VelocityData v;
+    double v = 0.0;
+    double w = 0.0;
+    double steering = 0.0;
+    if(msg->buttons[L1]){
+        v = msg->axes[L_STICK_V] * MAX_VELOCITY;
+        w = msg->axes[L_STICK_H] * MAX_ANGULAR_VELOCITY;
+        if(msg->buttons[L2] && !msg->buttons[R2]){
+            w = 0.0;
+            steering = MAX_STEERING_ANGLE * (1.0 - (msg->axes[L2_STICK] + 1.0) * 0.5);
+        }else if(msg->buttons[R2] && !msg->buttons[L2]){
+            w = 0.0;
+            steering = -MAX_STEERING_ANGLE * (1.0 - (msg->axes[R2_STICK] + 1.0) * 0.5);
+        }else if(msg->buttons[R2] && msg->buttons[L2]){
+            std::cout << "brake" << std::endl;
+            v = 0.0;
+            w = 0.0;
+        }
+    }else{
+        std::cout << "press L1 to move" << std::endl;
+    }
+    std::cout << "v: " << v << ", " << "w: " << w << ", " << "steering: " << steering << std::endl;
+    geometry_msgs::Twist cmd_vel;
+    cmd_vel.linear.x = v;
+    cmd_vel.angular.z = w;
+    cmd_vel_pub.publish(cmd_vel);
+    YPSpurWrapper::VelocityData vd;
 }
